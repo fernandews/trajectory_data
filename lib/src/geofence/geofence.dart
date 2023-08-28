d1361b6f1bd24f1c8b9e56805e646fbb6ef4b56f
Paula Fernandes <paula.bastos@quintoandar.com.br>
2023-08-28 10:23:25 -0300

    geofence ainda precisa de um foreground service no app

---
+++ lib/src/geofence/geofence.dart
@@ -0,0 +1,139 @@
+import 'package:geolocator/geolocator.dart';
+import 'dart:developer' as dev;
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+import 'package:trajectory_data/trajectory_data.dart';
+import 'package:geofence_service/geofence_service.dart';
+import 'dart:isolate';
+import 'dart:io';
+import 'dart:developer' as dev;
+
+class Geofencing {
+  // Create a [GeofenceService] instance and set options.
+  final _geofenceService = GeofenceService.instance.setup(
+      interval: 5000,
+      accuracy: 100,
+      loiteringDelayMs: 60000,
+      statusChangeDelayMs: 10000,
+      useActivityRecognition: false,
+      allowMockLocations: false,
+      printDevLog: false,
+      geofenceRadiusSortType: GeofenceRadiusSortType.DESC);
+
+  // Create a [Geofence] list.
+  final _geofenceList = <Geofence>[
+    Geofence(
+      id: 'place_1',
+      latitude: 35.103422,
+      longitude: 129.036023,
+      radius: [
+        GeofenceRadius(id: 'radius_100m', length: 100),
+        GeofenceRadius(id: 'radius_25m', length: 25),
+        GeofenceRadius(id: 'radius_250m', length: 250),
+        GeofenceRadius(id: 'radius_200m', length: 200),
+      ],
+    ),
+  ];
+
+  bool isRunningService = false;
+  void addEventHandlers () {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _geofenceService.addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
+      _geofenceService.addLocationChangeListener(_onLocationChanged);
+      _geofenceService.addLocationServicesStatusChangeListener(_onLocationServicesStatusChanged);
+      _geofenceService.addStreamErrorListener(_onError);
+      _geofenceService.start(_geofenceList).catchError(_onError);
+    });
+  }
+}
+
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(FirstTaskHandler());
+}
+
+class FirstTaskHandler extends TaskHandler {
+  SendPort? _sendPort;
+
+  // Called when the task is started.
+  @override
+  Future<void> onStart(DateTime timestamp, SendPort? sendPort) async {
+    _sendPort = sendPort;
+
+    // You can use the getData function to get the stored data.
+    final customData =
+    await FlutterForegroundTask.getData<String>(key: 'customData');
+    dev.log('customData: $customData');
+  }
+
+  // Called every [interval] milliseconds in [ForegroundTaskOptions].
+  @override
+  Future<void> onRepeatEvent(DateTime timestamp, SendPort? sendPort) async {
+    // Send data to the main isolate.
+    sendPort?.send(timestamp);
+  }
+
+
+  @override
+  Future<void> onDestroy(DateTime timestamp, SendPort? sendPort) async {
+
+  }
+
+  @override
+  Future<void> onEvent(DateTime timestamp, SendPort? sendPort) async {
+
+  }
+
+  // Called when the notification button on the Android platform is pressed.
+  @override
+  void onNotificationButtonPressed(String id) {
+    dev.log('onNotificationButtonPressed >> $id');
+  }
+
+  // Called when the notification itself on the Android platform is pressed.
+  //
+  // "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted for
+  // this function to be called.
+  @override
+  void onNotificationPressed() {
+    // Note that the app will only route to "/resume-route" when it is exited so
+    // it will usually be necessary to send a message through the send port to
+    // signal it to restore state when the app is already started.
+    FlutterForegroundTask.launchApp("/resume-route");
+    _sendPort?.send('onNotificationPressed');
+  }
+}
+
+// This function is to be called when the geofence status is changed.
+Future<void> _onGeofenceStatusChanged(
+    Geofence geofence,
+    GeofenceRadius geofenceRadius,
+    GeofenceStatus geofenceStatus,
+    Location location) async {
+  print('geofence: ${geofence.toJson()}');
+  print('geofenceRadius: ${geofenceRadius.toJson()}');
+  print('geofenceStatus: ${geofenceStatus.toString()}');
+}
+
+// This function is to be called when the location has changed.
+void _onLocationChanged(Location location) {
+  print('location: ${location.toJson()}');
+}
+
+// This function is to be called when a location services status change occurs
+// since the service was started.
+void _onLocationServicesStatusChanged(bool status) {
+  print('isLocationServicesEnabled: $status');
+}
+
+// This function is used to handle errors that occur in the service.
+void _onError(error) {
+  final errorCode = getErrorCodesFromError(error);
+  if (errorCode == null) {
+    print('Undefined error: $error');
+    return;
+  }
+
+  print('ErrorCode: $errorCode');
+}
--- lib/trajectory_data.dart
+++ lib/trajectory_data.dart
@@ -1,4 +1,5 @@
 library trajectory_data;
 
 export 'package:trajectory_data/src/geolocation/geolocation.dart';
+export  'package:trajectory_data/src/geofence/geofence.dart';
 
